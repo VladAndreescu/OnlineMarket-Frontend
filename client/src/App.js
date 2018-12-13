733dd212e29ffabf16e6e16a653fb771aeaf6a54
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-13 16:43:11 +0000

    first implementation of NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>MyStuff</h1>
+        <Navbar/>
+		<h1>MyStaff</h1>
       </div>
     );
   }
