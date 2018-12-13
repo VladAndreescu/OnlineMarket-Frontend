42b57e2df2b4e2b25b974c4f5fd7f0e56e9470e2
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-13 22:34:54 +0000

    Implemented automate logout if the token expiration time has passed

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import store from './store'
 
 //import functions
 import setAuthenticationToken from './utils/setAuthenticationToken'
-import {setCurrentUser} from './actions/authActions'
+import {setCurrentUser, userLogout} from './actions/authActions'
 
 //import CSS
 import './App.css';
@@ -33,6 +33,16 @@ if(localStorage.jwt_token){
 
 	//Set the value for user and isAuthenticated
 	store.dispatch(setCurrentUser(decoded))
+
+	//Check if the expiration time has passed
+	const current = Date.now() / 1000
+	if(decoded.exp < current){
+		//Logout the user
+		store.dispatch(userLogout())
+
+		//Redirect user to login page
+		window.location.href = '/login'
+	}
 }else{
 	console.log('No user is currently logged in')
 }
