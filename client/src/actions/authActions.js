8488c81bd78ecb93da44b16f1fee53ce5459c1d0
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-13 21:44:48 +0000

    Get Authentication Token function was created

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,11 @@
 //import utils
-import {GET_ERRORS} from './types'
 import axios from 'axios'
+import setAuthenticationToken from '../utils/setAuthenticationToken'
+import jwt_decode from 'jwt-decode'
+
+//import types
+import {GET_ERRORS} from './types'
+
 
 //Register User
 export const userRegister = (userData, history) => dispatch =>{
@@ -14,5 +19,30 @@ export const userRegister = (userData, history) => dispatch =>{
 				payload: err.response.data
 			})	
 		)
+}
+
+//Authenticate User and get hes unique Token
+export const userAuthenticate = userData => dispatch =>{
+	//make a POST request to the database in order to authenticate the user
+	axios.post('/api/users/login', userData)
+	.then(res =>{
+		//use destructuring to retrieve the token from the user data
+		const {token} = res.data
+
+		// Save token in localStorage
+		localStorage.setItem('jwt_token', token);
+
+		//Insert Token into the Authorization token
+		setAuthenticationToken(token);
+		
+		//Decode the token in order to receive the user information
+		const decoded = jwt_decode(token);
+	})
+	.catch(err =>{
+		dispatch({
+			type: GET_ERRORS,
+			payload: err.response.data
+		})
+	})
 }
 
