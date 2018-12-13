7bb2136f6ca14ad83f6a456aba4b0157c75720e1
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-13 17:40:52 +0000

    Implemented Link utility in order to link the buttons to specific paths

--- client/src/components/layout/Home.js
+++ client/src/components/layout/Home.js
@@ -1,4 +1,6 @@
+//import utils
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 class Home extends Component {
   render() {
@@ -11,8 +13,8 @@ class Home extends Component {
 				<h1 className="display-3 mb-4">MyStuff Online listing App</h1>
 				<p className="lead"> Listing an item requires an account. Please create one or Login</p>
 				<hr />
-				<a href="register.html" className="btn btn-outline-primary btn-lg mr-2">Register</a>
-				<a href="login.html" className="btn btn-outline-light btn-lg">Login</a>
+				<Link to="/register" className="btn btn-outline-primary btn-lg mr-2">Register</Link>
+				<Link to="/login" className="btn btn-outline-light btn-lg">Login</Link>
 			  </div>
 			</div>
 		  </div>
