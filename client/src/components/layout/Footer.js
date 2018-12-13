51f08e0230e597351ca3ad04870e3e540aae082a
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-13 16:50:22 +0000

    Implemented Navbar and now starting Footer

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => {
+  return (
+	<footer className="bg-dark text-white p-3 mt-6 text-center"></footer>
+  )
+}
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,9 +3,31 @@ import React, { Component } from 'react'
 class Navbar extends Component {
   render() {
 	return (
-	  <div>
-		<h1>test navbar</h1>
-	  </div>
+		<nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+    		<div class="container">
+      			<a class="navbar-brand" href="landing.html">MyStuff</a>
+      			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+        			<span class="navbar-toggler-icon"></span>
+      			</button>
+
+      			<div class="collapse navbar-collapse" id="mobile-nav">
+        			<ul class="navbar-nav mr-auto">
+          				<li class="nav-item">
+            				<a class="nav-link" href="profiles.html"> Items </a>
+         				 </li>
+        			</ul>
+
+        			<ul class="navbar-nav ml-auto">
+          				<li class="nav-item">
+            				<a class="nav-link" href="register.html">Register</a>
+          				</li>
+          				<li class="nav-item">
+            				<a class="nav-link" href="login.html">Login</a>
+          				</li>
+        			</ul>
+      			</div>
+    		</div>
+  		</nav>
 	)
   }
 }
