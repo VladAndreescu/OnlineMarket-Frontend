6fc8cef51c358a7e21db495d8c1da13a21d3f3cd
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-13 16:53:33 +0000

    Implemented footer component

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default () => {
   return (
-	<footer className="bg-dark text-white p-3 mt-6 text-center"></footer>
+	<footer className="bg-dark text-white p-3 mt-6 text-center">
+		MyStuff application Copyright &copy; {new Date().getFullYear()}
+	</footer>
   )
 }
