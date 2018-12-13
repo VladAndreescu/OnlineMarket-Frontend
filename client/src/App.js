f63e773d22fdc8afb0d84e85c1498ad7ed4b9560
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-13 20:34:51 +0000

    Redux implemented and store was created

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@
 //import utils
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
 
 // import components
 import Navbar from './components/layout/Navbar'
@@ -12,25 +14,30 @@ import Register from './components/authentication/Register'
 
 import './App.css';
 
+//creating the store for Redux
+const  store = createStore(() => [], {}, applyMiddleware())
+
 class App extends Component {
   render() {
     return (
-      <Router>
-		<div className="App">
-			<Navbar/>
-			 {
-				 //surrounding the Home component with a Route because I want to display it only on a single path
-			 }
-			<Route  exact path="/" component={Home}/> 
+		<Provider store = {store}>
+			<Router>
+				<div className="App">
+					<Navbar/>
+					{
+						//surrounding the Home component with a Route because I want to display it only on a single path
+					}
+					<Route  exact path="/" component={Home}/> 
 
-			<div className="container">
-				 <Route exact path="/register" component = {Register} />
-				 <Route exact path="/login" component ={Login} />
-				 
-			</div>
-			<Footer/>
-		</div>
-	  </Router>
+					<div className="container">
+						<Route exact path="/register" component = {Register} />
+						<Route exact path="/login" component ={Login} />
+						
+					</div>
+					<Footer/>
+				</div>
+			</Router>
+	  </Provider>
     );
   }
 }
