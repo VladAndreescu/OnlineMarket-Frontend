ed024ef33071fab60d54e6a860d145249942ae14
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-13 17:36:11 +0000

    Created Login and Register Components

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Home from './components/layout/Home'
+import Login from  './components/authentication/Login'
+import Register from './components/authentication/Register'
+
 import './App.css';
 
 class App extends Component {
@@ -19,6 +22,12 @@ class App extends Component {
 				 //surrounding the Home component with a Route because I want to display it only on a single path
 			 }
 			<Route  exact path="/" component={Home}/> 
+
+			<div className="container">
+				 <Route exact path="/register" component = {Register} />
+				 <Route exact path="/login" component ={Login} />
+				 
+			</div>
 			<Footer/>
 		</div>
 	  </Router>
